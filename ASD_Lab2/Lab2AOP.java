41b2b60838f386e4ddaba73fbdf708903e2d2ae6
KondratovIvan <kondratovivanqq@gmail.com>
2022-02-17 19:46:58 +0200

    Create Lab2AOP.java

---
+++ ASD_Lab2/Lab2AOP.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Paralelepiped{
+	private double vvod;
+	
+	Paralelepiped(){}
+	Paralelepiped(double choice){vvod = choice;}
+}
+public class Lab2AOP {
+
+	 public static void main(String[] args) {
+		 Scanner scan = new Scanner(System.in);
+		 System.out.println("0. Вихід");
+		 System.out.println("1. Ввести перше ребро");
+		 System.out.println("2. Ввести друге ребро");
+		 System.out.println("3. Ввести третє ребро");
+		 System.out.println("4. Обчислити суму площ граней паралелепіпеда");
+		 System.out.println("5. Обчислити об'єм паралелепіпеда");
+		 int choice=scan.nextInt();
+		 switch(choice) {
+		 case 0:
+		 case 1:
+		 case 2:
+		 case 3:
+		 case 4:
+		 case 5:
+		 }
+	 }
+}
